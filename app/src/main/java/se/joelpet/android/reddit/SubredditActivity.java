b9b10ed7f5c4a3362725214c780bba57a0be075c
Joel Pettersson <petterssonjoel@gmail.com>
2014-10-04 00:50:39 +0200

    Fetch top title from Reddit

--- app/src/main/java/se/joelpet/android/reddit/SubredditActivity.java
+++ app/src/main/java/se/joelpet/android/reddit/SubredditActivity.java
@@ -1,17 +1,26 @@
 package se.joelpet.android.reddit;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SubredditActivity extends Activity {
 
@@ -25,7 +34,6 @@ public class SubredditActivity extends Activity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,14 +59,53 @@ public class SubredditActivity extends Activity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private TextView textView1;
+
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_subreddit, container, false);
+            textView1 = (TextView) rootView.findViewById(R.id.textView1);
             return rootView;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            RequestQueue queue = Volley.newRequestQueue(getActivity());
+            JSONObject jsonObject = new JSONObject();
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                    "http://www.reddit.com/top.json", jsonObject, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject jsonObject) {
+                    System.out.print(jsonObject);
+                    try {
+                        JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("children");
+                        String title = ((JSONObject) jsonArray.get(0)).getJSONObject("data").getString("title");
+                        textView1.setText(title);
+                        Log.d(SubredditActivity.class.getSimpleName(),
+                                String.format("Fetched top post and updated title to '%s'", title));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    System.out.print(volleyError.toString());
+                }
+            });
+
+            queue.add(jsonObjectRequest);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
         }
     }
 }
